f59a91f7ef91e1be607ca1d078cc775ccfd0b0c5
Coffi Tinhoueto <coffi.tinhoueto@gmail.com>
2023-05-03 12:41:58 -0400

    created vpc file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         ="c-terraform-remote-state"
+    bucket         = "c-terraform-remote-state"
     key            = "rentzone-ecs/terraform.tfstate"
     region         = "us-east-1"
     profile        = "ChrisTmyprofile"
